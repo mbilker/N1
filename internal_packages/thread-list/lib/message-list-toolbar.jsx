8d2ab8956826bdbefa3d79d4c85b35873adbf840
mbilker <me@mbilker.us>
2016-05-08 17:17:13 -0400

    fix(thread-list): expose a displayName property

--- internal_packages/thread-list/lib/message-list-toolbar.jsx
+++ internal_packages/thread-list/lib/message-list-toolbar.jsx
@@ -1,29 +1,25 @@
-import Rx from 'rx-lite'
-import React, {PropTypes} from 'react'
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
-import {FocusedContentStore} from 'nylas-exports'
-import ThreadListStore from './thread-list-store'
-import InjectsToolbarButtons, {ToolbarRole} from './injects-toolbar-buttons'
-
+import Rx from 'rx-lite';
+import React, {PropTypes} from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import {FocusedContentStore} from 'nylas-exports';
+import ThreadListStore from './thread-list-store';
+import InjectsToolbarButtons, {ToolbarRole} from './injects-toolbar-buttons';
 
 function getObservable() {
-  return (
-    Rx.Observable.combineLatest(
-      Rx.Observable.fromStore(FocusedContentStore),
-      ThreadListStore.selectionObservable(),
-      (store, items) => ({focusedThread: store.focused('thread'), items})
-    )
-    .map(({focusedThread, items}) => {
-      if (focusedThread) {
-        return [focusedThread]
-      }
-      return items
-    })
-  )
-}
+  return Rx.Observable.combineLatest(
+    Rx.Observable.fromStore(FocusedContentStore),
+    ThreadListStore.selectionObservable(),
+    (store, items) => ({focusedThread: store.focused('thread'), items})
+  ).map(({focusedThread, items}) => {
+    if (focusedThread) {
+      return [focusedThread];
+    }
+    return items;
+  });
+};
 
 const MessageListToolbar = ({items, injectedButtons}) => {
-  const shouldRender = items.length > 0
+  const shouldRender = items.length > 0;
 
   return (
     <ReactCSSTransitionGroup
@@ -34,8 +30,10 @@ const MessageListToolbar = ({items, injectedButtons}) => {
     >
       {shouldRender ? injectedButtons : undefined}
     </ReactCSSTransitionGroup>
-  )
-}
+  );
+};
+
+MessageListToolbar.displayName = 'MessageListToolbar';
 
 MessageListToolbar.propTypes = {
   items: PropTypes.array,
@@ -45,6 +43,6 @@ MessageListToolbar.propTypes = {
 const toolbarProps = {
   getObservable,
   extraRoles: [`MessageList:${ToolbarRole}`],
-}
+};
 
-export default InjectsToolbarButtons(MessageListToolbar, toolbarProps)
+export default InjectsToolbarButtons(MessageListToolbar, toolbarProps);
